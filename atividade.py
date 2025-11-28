a02a3fc6531bddf93fa7a2150320c8b2c7db8210
LucasIbrahinCardoso <lucasibraca@gmail.com>
2025-11-28 17:32:27 -0400

    não consegui fazer tudo ;-;

--- atividade.py
+++ atividade.py
@@ -30,6 +30,43 @@ for a in range(len(nomes)):
         else:
             print("Resposta inválida, tente 'lido', 'lendo' ou 'fila':")
 
-with open("livros_avaliacao.txt", "w") as arq:
+with open("livros_avaliacao.txt", "w", encoding="utf-8") as arq:
     for um in range(len(texto)):
-        arq.write(f"{anos[um]}, {nomes[um]}, {autor[um]}, {notas[um]}, {status[um]}")
+        arq.write(f"{anos[um]}, {nomes[um]}, {autor[um]}, {notas[um]}, {status[um]}\n")
+    
+avaliadosPrincipal = funcoes.leitor("livros_avaliacao.txt")
+avaliados = []
+for i in range(len(avaliadosPrincipal)):
+    avaliados.append(avaliadosPrincipal[i].removesuffix("\n"))
+for j in range(len(avaliados)):
+    avaliados[j] = avaliados[j].split(",")
+
+verificando = []
+for teste in range(len(avaliados)):
+    if avaliados[teste][4] != ' fila':
+        verificando.append([avaliados[teste][1], avaliados[teste][3]])
+
+for i in range(len(verificando)):
+    verificando[i][1] = int(verificando[i][1])
+
+notasLimpas = []
+for lal in range(len(verificando)):
+    notasLimpas.append(verificando[lal][1])
+
+notasLimpas = sorted(notasLimpas, reverse=True)
+
+notasFinais= []
+for la in range(0, 5, 1):
+    notasFinais.append(notasLimpas[la])
+
+listaFinal = []
+for ti in range(9):
+    for le in range(len(verificando)):
+        for teste in range(len(notasFinais)):
+            if notasFinais[teste] != verificando[le]:
+                notasFinais.remove(notasFinais[teste])
+            else:
+                listaFinal.append(verificando[le][0])
+                if len(listaFinal) == 5:
+                    break
+print(listaFinal)
